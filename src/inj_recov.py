bugfix normalization to divide, not subtract
Bugfix also included an inclination problem in previous injections -- I
was giving batman an inclination in radians, not degrees. (And it wants
degrees).

N.b. this commit also moves parsing of injection/recovery results to
CSV files to injrecoverresult_analysis.py.

src/inj_recov.py
@@ -166,7 +166,7 @@ def inject_transit_known_depth(lcd, δ):
 
     params.a = a_by_Rstar
     inc = np.arccos(cosi)
-    params.inc = inc
+    params.inc = np.rad2deg(inc)
 
     exp_time_minutes = 29.423259
     exp_time_days = exp_time_minutes / (24.*60)
@@ -221,8 +221,8 @@ def inject_transit_known_depth(lcd, δ):
         kbegin += len(qtimes)
 
     kicid = str(lcd[qnum]['objectinfo']['keplerid'])
-    LOGINFO('KICID {:s}: injected transit to both SAP and PDC fluxes.'.\
-            format(kicid))
+    LOGINFO('KICID {:s}: injected {:f} transit to both SAP and PDC fluxes.'.\
+            format(kicid, δ))
 
     return lcd, allq
 
@@ -943,7 +943,9 @@ def normalize_lightcurve(lcd, qnum, dt='dtr'):
         fitflux = lcd[dt][ap][dtrtype]
         fitflux_norm = fitflux / np.median(flux)
 
-        flux_dtr_norm = flux_norm - fitflux_norm + 1
+        #NOTE: there is some abiguity about whether to divide by the fit
+        #(preserving relative variability) or to subtract. Trying divide.
+        flux_dtr_norm = flux_norm / fitflux_norm
 
         errs = lcd[dt][ap]['errs']
         errs_dtr_norm =  errs / np.median(flux)
@@ -1614,114 +1616,6 @@ def load_allq_data(kicid, stage=None):
     LOGINFO('Loaded allquarter pickled data from %s' % lpath)
 
     return allq
-
-
-def write_injrecov_result(lcd, allq, stage=None):
-    '''
-    Append the result of this injection-recovery experiment (i.e. whether it
-    was successful, what the basic parameters of the EB and the injected system
-    were, what the basic parameters of the LC were) to csv files.
-
-    A system is discovered if its "fine" (rather than coarse) best period and
-    best transit epoch agree with the injected ones to some precision.
-
-    There are two files:
-    csv1: rows are results with only best periodogram period for each system.
-    csv2: rows are results with all 5 "best guesses" at period for each system.
-
-    These csv files are aperture specific.
-    '''
-
-    kicid = lcd[list(lcd.keys())[0]]['objectinfo']['keplerid']
-    kebc_period = float(lcd[list(lcd.keys())[0]]['kebwg_info']['period'])
-    morph = float(lcd[list(lcd.keys())[0]]['kebwg_info']['morph'])
-    im = allq['inj_model']
-    P_inj = im['params'].per
-    t0_inj = im['params'].t0
-    δ = (im['params'].rp)**2
-
-    csvdir = '../results/injrecovresult/'
-
-    for ap in ['sap','pdc']:
-        csv1name = 'irresult_'+ap+'_'+stage+'_top1.csv'
-        csv2name = 'irresult_'+ap+'_'+stage+'_allN.csv'
-
-        # Get minimum time for epoch zero-point
-        lc = allq['dipfind']['tfe'][ap]
-        min_time = np.min(lc['times'])
-        fluxs = lc['fluxs']
-        meanflux = np.mean(fluxs)
-        rms_biased = float(np.sqrt(np.sum((fluxs-meanflux)**2) / len(fluxs)))
-
-        # Recover best period
-        pgdc = allq['dipfind']['bls'][ap]['coarsebls']
-        pgdf = allq['dipfind']['bls'][ap]['finebls']
-        cnbestperiods = np.sort(pgdc['nbestperiods'])
-        cbestperiod = pgdc['bestperiod']
-        fnbestperiods = np.sort([pgdf[cnbp]['serialdict']['bestperiod']
-                for cnbp in cnbestperiods])
-        fbestperiod = pgdf[cbestperiod]['serialdict']['bestperiod']
-        bestperiod = fbestperiod
-
-        for ix, ffoldperiod in enumerate(fnbestperiods):
-
-            cfoldperiod = cnbestperiods[ix]
-            fbls = allq['dipfind']['bls'][ap]['finebls'][cfoldperiod]
-            φ_0 = fbls['φ_0']
-
-            t0_rec = min_time + φ_0*ffoldperiod
-            P_rec = ffoldperiod
-
-            # If the recovered epoch and period are within +/- 0.1 days of the
-            # injected epoch and period, we "recovered" the injected signal.
-            tol = 0.1
-            if (abs(P_inj - P_rec) < tol) and (abs(t0_inj - t0_rec) < tol):
-                foundinj = True
-            else:
-                foundinj = False
-
-            results = pd.DataFrame({
-                    'kicid':kicid,
-                    'kebc_period':kebc_period,
-                    'morph':morph,
-                    'ap':ap,
-                    'P_inj':P_inj,
-                    'P_rec':P_rec,
-                    'coarseperiod':cfoldperiod,
-                    't0_inj':t0_inj,
-                    't0_rec':t0_rec,
-                    'foundinj':foundinj,
-                    'rms_biased':rms_biased,
-                    'depth':δ
-                    }, index=['0'])
-
-            # Write csv1 (appending if the csv file already exists)
-            if ffoldperiod == bestperiod:
-                if not os.path.isfile(csvdir+csv1name):
-                    results.to_csv(csvdir+csv1name,
-                            header=True,
-                            index=False)
-                else:
-                    results.to_csv(csvdir+csv1name,
-                            header=False,
-                            index=False,
-                            mode='a')
-
-            # Write csv2
-            if not os.path.isfile(csvdir+csv2name):
-                results.to_csv(csvdir+csv2name,
-                        header=True,
-                        index=False)
-            else:
-                results.to_csv(csvdir+csv2name,
-                        header=False,
-                        index=False,
-                        mode='a')
-
-            LOGINFO('Wrote KIC-{:d} result to {:s} ({:s})'.format(
-                kicid,csvdir,ap))
-
-
 
 
 def drop_gaps_in_lightcurves(times, mags, errs):