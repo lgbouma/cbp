9f3cfc86de56a6976530944683e9730f55ee42f4
lgbouma <lgbouma@mit.edu>
2017-03-26 09:37:11 -0400

    refactor run_inj_recov to use argparse
    Added argparse to clarify control flow logic.
    
    This refactoring mostly was just deleting deprecated routines that I
    needed to write to test whether the iterative whitening (and other steps
    of the inj/recov pipeline) worked.  It does, so they can now be deleted.

--- src/run_inj_recov.py
+++ src/run_inj_recov.py
@@ -2,7 +2,7 @@ import numpy as np, os
 import inj_recov as ir
 import inj_recov_plots as irp
 import injrecovresult_analysis as irra
-
+import argparse
 
 def get_lcd(stage='redtr', inj=None, allq=None):
     '''
@@ -31,346 +31,6 @@ def get_lcd(stage='redtr', inj=None, allq=None):
         return lcd
 
 
-def injrecov_fixed_transit_depth(N,
-        ors=False, whitened=True, ds=True,
-        stage=None,
-        inj=None):
-    '''
-    Run injection-recovery on N KEBC objects. There are two important objects:
-    `lcd` organizes everything by quarter. `allq` stitches over all quarters.
-
-    The processing implemented in this routine is as follows:
-
-        inject a transit δ = 1% at (20-25)x the binary
-            period->
-        detrend (n=20 legendre and [30,30]σ sigclip)->
-        normalize (median by quarter)->
-        run periodograms (stellingwerf_pdm)->
-        get nominal EB period->
-        run finer periodogram (stellingwerf_pdm narrow window)->
-        select a real EB period->
-        "whiten" (phase-fold [30,5]σ sigclip, fit & subtract out fit)->
-        redetrend (n=20 legendre, and [30,5]σ  sigclip)->
-        normalize (median by quarter)->
-        find dips (BLS, over all the quarters)
-
-    stages (in order that they could be saved in):
-
-        'pw' if post-whitening.
-        'redtr' if post-redetrending.
-        'ds' if up to recovering the dips.
-
-    inj (bool): True if you're injecting (fixes names of things).
-
-    ors (bool), whitened (bool), ds (bool): whether to create the Orosz,
-    whitened 6row, and dipsearch plots respectively.
-    '''
-
-    np.random.seed(N)
-    seeds = np.random.randint(0, 99999999, size=N)
-
-    if inj:
-        stage += '_inj'
-    predir = ''
-    if 'inj' in stage:
-        predir += 'inj/'
-    elif 'inj' not in stage:
-        predir += 'no_inj/'
-
-
-    for s in seeds:
-        np.random.seed(s)
-
-        lcd, lcflag = ir.retrieve_random_lc()
-        if lcflag:
-            continue
-        kicid = str(lcd[list(lcd.keys())[0]]['objectinfo']['keplerid'])
-
-        pklmatch = [f for f in os.listdir('../data/injrecov_pkl/'+predir) if
-                f.endswith('.p') and f.startswith(kicid) and stage in f]
-
-        if len(pklmatch) < 1:
-
-            #INJECT TRANSITS AND PROCESS LIGHTCURVES
-            δ = 0.01
-            lcd, allq = ir.inject_transit_known_depth(lcd, δ)
-            lcd = ir.detrend_allquarters(lcd,
-                    σ_clip=30., legendredeg=20, inj=inj)
-            lcd = ir.normalize_allquarters(lcd, dt='dtr')
-            lcd = ir.run_periodograms_allquarters(lcd)
-            lcd = ir.select_eb_period(lcd, fine=False)
-            lcd = ir.run_fineperiodogram_allquarters(lcd)
-            lcd = ir.select_eb_period(lcd, fine=True)
-            lcd = ir.whiten_allquarters(lcd, σ_clip=[30.,5.], iter_n=0)
-            if 'pw' in stage:
-                kicid = ir.save_lightcurve_data(lcd, stage=stage)
-            lcd = ir.redetrend_allquarters(lcd, σ_clip=[30.,5.], legendredeg=20)
-            lcd = ir.normalize_allquarters(lcd, dt='redtr')
-            if 'redtr' in stage:
-                kicid = ir.save_lightcurve_data(lcd, stage=stage)
-            allq = ir.find_dips(lcd, allq, method='bls')
-            if 'dipsearch' in stage:
-                kicid = ir.save_lightcurve_data(lcd, allq=allq, stage=stage)
-
-        #LOAD STUFF
-        lcd, loadfailed = ir.load_lightcurve_data(kicid, stage=stage)
-        if 'dipsearch' in stage:
-            allq, loadfailed = ir.load_allq_data(kicid, stage=stage)
-
-        #WRITE RESULTS TABLES
-        if 'dipsearch' in stage:
-            if inj:
-                ir.write_injrecov_result(lcd, allq, stage=stage)
-
-        #MAKE PLOTS
-        if ors:
-            irp.orosz_style_flux_vs_time(lcd, flux_to_use='sap', stage=stage)
-            irp.orosz_style_flux_vs_time(lcd, flux_to_use='pdc', stage=stage)
-
-        if ds:
-            doneplots = os.listdir('../results/dipsearchplot/'+predir)
-            plotmatches = [f for f in doneplots if f.startswith(kicid) and
-                    stage in f]
-            if len(plotmatches)>0:
-                print('\nFound dipsearchplot, continuing.\n')
-                continue
-
-            if 'dipsearch' in stage:
-                irp.dipsearchplot(lcd, allq, ap='sap', stage=stage, inj=inj)
-                irp.dipsearchplot(lcd, allq, ap='pdc', stage=stage, inj=inj)
-
-        if whitened:
-            doneplots = os.listdir('../results/whitened_diagnostic/'+predir)
-            plotmatches = [f for f in doneplots if f.startswith(kicid) and
-                    stage in f]
-            if len(plotmatches)>0:
-                print('\nFound whitened_diagnostic, continuing.\n')
-                continue
-
-            if 'pw' in stage:
-                irp.whitenedplot_5row(lcd, ap='sap', stage=stage)
-                irp.whitenedplot_5row(lcd, ap='pdc', stage=stage)
-            elif 'redtr' in stage:
-                irp.whitenedplot_6row(lcd, ap='sap', stage=stage, inj=inj)
-                irp.whitenedplot_6row(lcd, ap='pdc', stage=stage, inj=inj)
-            elif 'dipsearch' in stage:
-                irp.whitenedplot_6row(lcd, ap='sap', stage=stage, inj=inj)
-                irp.whitenedplot_6row(lcd, ap='pdc', stage=stage, inj=inj)
-
-    if inj:
-        irra.summarize_injrecov_result()
-
-
-def injrecov_vary_depth(N,
-        ors=False, whitened=True, ds=True,
-        stage=None,
-        inj=None):
-    '''
-    Inject transits and recover them on N entries from the Kepler Eclipsing
-    Binary Catalog. There are two important objects:
-    `lcd` organizes everything by quarter. `allq` stitches over all quarters.
-
-    Currently implemented:
-
-        inject a realistic transit signal at δ=(2,1,1/2,1/4,1/8,1/16,1/32)%
-            depth, anywhere from P_CBP=(4-40)x P_EB.
-        detrend (n=20 legendre and [30,30]σ sigclip)->
-        normalize (median by quarter)->
-        run periodograms (stellingwerf_pdm)->
-        get nominal EB period->
-        run finer periodogram (stellingwerf_pdm narrow window)->
-        select a real EB period->
-        "whiten" (phase-fold [30,5]σ sigclip, fit & subtract out fit)->
-        redetrend (n=20 legendre, and [30,5]σ  sigclip)->
-        normalize (median by quarter)->
-        find dips (BLS, over all the quarters)
-
-    stages (in order that they could be saved in):
-
-        'pw' if post-whitening.
-        'redtr' if post-redetrending.
-        'ds' if up to recovering the dips.
-
-    inj (bool): True if you're injecting (fixes names of things).
-
-    ors (bool), whitened (bool), ds (bool): whether to create the Orosz,
-    whitened 6row, and dipsearch plots respectively.
-    '''
-
-    np.random.seed(N)
-    seeds = np.random.randint(0, 99999999, size=N)
-
-    if inj:
-        stage += '_inj'
-    predir = ''
-    if 'inj' in stage:
-        predir += 'inj/'
-    elif 'inj' not in stage:
-        predir += 'no_inj/'
-
-    origstage = stage
-
-    for s in seeds:
-        np.random.seed(s)
-
-        lcd, lcflag = ir.retrieve_random_lc()
-        if lcflag:
-            continue
-        kicid = str(lcd[list(lcd.keys())[0]]['objectinfo']['keplerid'])
-
-        #INJECT TRANSITS AND PROCESS LIGHTCURVES
-        δarr = np.array([2.,1.,1/2.,1/4.,1/8.,1/16.])/100.
-
-        for δ in δarr:
-            stage = origstage + '_' + str(δ)
-            pklmatch = [f for f in os.listdir('../data/injrecov_pkl/'+predir) if
-                    f.endswith('.p') and f.startswith(kicid) and stage in f]
-            if len(pklmatch) > 0:
-                print('Found {:s}, {:f}, continue'.format(kicid, δ))
-                continue
-            lcd, allq = ir.inject_transit_known_depth(lcd, δ)
-            lcd = ir.detrend_allquarters(lcd,
-                    σ_clip=30., legendredeg=20, inj=inj)
-            lcd = ir.normalize_allquarters(lcd, dt='dtr')
-            lcd = ir.run_periodograms_allquarters(lcd, iter_n=0)
-            lcd = ir.select_eb_period(lcd, fine=False)
-            lcd = ir.run_fineperiodogram_allquarters(lcd, iter_n=0)
-            lcd = ir.select_eb_period(lcd, fine=True)
-            lcd = ir.whiten_allquarters(lcd, σ_clip=[30.,5.], iter_n=0)
-            if 'pw' in stage:
-                kicid = ir.save_lightcurve_data(lcd, stage=stage)
-            lcd = ir.redetrend_allquarters(lcd, σ_clip=[30.,5.], legendredeg=20)
-            lcd = ir.normalize_allquarters(lcd, dt='redtr')
-            if 'redtr' in stage:
-                kicid = ir.save_lightcurve_data(lcd, stage=stage)
-            allq = ir.find_dips(lcd, allq, method='bls')
-            if 'dipsearch' in stage:
-                kicid = ir.save_lightcurve_data(lcd, allq=allq, stage=stage)
-
-
-        for δ in δarr:
-            stage = origstage + '_' + str(δ)
-            #LOAD STUFF
-            lcd, loadfailed = ir.load_lightcurve_data(kicid, stage=stage)
-            if 'dipsearch' in stage:
-                allq, loadfailed = ir.load_allq_data(kicid, stage=stage)
-
-            #WRITE RESULTS TABLES
-            if 'dipsearch' in stage:
-                if inj:
-                    irra.write_injrecov_result(lcd, allq, stage=stage)
-
-            #MAKE PLOTS
-            if ors:
-                irp.orosz_style_flux_vs_time(lcd, flux_to_use='sap', stage=stage)
-                irp.orosz_style_flux_vs_time(lcd, flux_to_use='pdc', stage=stage)
-
-            if ds:
-                doneplots = os.listdir('../results/dipsearchplot/'+predir)
-                plotmatches = [f for f in doneplots if f.startswith(kicid) and
-                        stage in f]
-                if len(plotmatches)>0:
-                    print('\nFound dipsearchplot, continuing.\n')
-                    continue
-
-                if 'dipsearch' in stage:
-                    irp.dipsearchplot(lcd, allq, ap='sap', stage=stage, inj=inj)
-                    irp.dipsearchplot(lcd, allq, ap='pdc', stage=stage, inj=inj)
-
-            if whitened:
-                doneplots = os.listdir('../results/whitened_diagnostic/'+predir)
-                plotmatches = [f for f in doneplots if f.startswith(kicid) and
-                        stage in f]
-                if len(plotmatches)>0:
-                    print('\nFound whitened_diagnostic, continuing.\n')
-                    continue
-
-                if 'pw' in stage:
-                    irp.whitenedplot_5row(lcd, ap='sap', stage=stage)
-                    irp.whitenedplot_5row(lcd, ap='pdc', stage=stage)
-                elif 'redtr' in stage:
-                    irp.whitenedplot_6row(lcd, ap='sap', stage=stage, inj=inj)
-                    irp.whitenedplot_6row(lcd, ap='pdc', stage=stage, inj=inj)
-                elif 'dipsearch' in stage:
-                    irp.whitenedplot_6row(lcd, ap='sap', stage=stage, inj=inj)
-                    irp.whitenedplot_6row(lcd, ap='pdc', stage=stage, inj=inj)
-
-        if inj:
-            irra.summarize_injrecov_result()
-
-
-def test_EB_subraction(N,
-        ors=False, whitened=True, ds=True,
-        stage=None,
-        inj=None,
-        nwhiten_max=8,
-        nwhiten_min=1):
-    '''
-    Calls routines for testing and improving subtraction of the eclipsing
-    binary signal.
-
-    Current subtraction method:
-        Iterative whitening via PDM period-selection and polynomial fitting.
-
-    Args:
-        stage (str): 'eb_sbtr'.
-        nwhiten_max (int): maximum number of iterative whitenings to do.
-        nwhiten_min (int): minimum number of iterative whitenings to do.
-
-    For other args / kwargs, see docstrings to injrecov_vary_depth and/or
-    injrecov_fixed_transit_depth.
-    '''
-
-    np.random.seed(N)
-    seeds = np.random.randint(0, 99999999, size=N)
-
-    stage = stage+'_inj' if inj else stage
-    predir = 'inj/' if 'inj' in stage else 'no_inj/'
-    origstage = stage
-
-    for s in seeds:
-        np.random.seed(s)
-
-        lcd, lcflag = ir.retrieve_random_lc()
-        kicid = str(lcd[list(lcd.keys())[0]]['objectinfo']['keplerid'])
-
-        # Nominal 0.125% transit injection.
-        δ = 1/8./100.
-        # Control flow for injection & iterative whitening.
-        if lcflag:
-            continue
-        stage = origstage + '_' + str(δ)
-        pklmatch = [f for f in os.listdir('../data/eb_subtr_pkl') if
-                f.endswith('.p') and f.startswith(kicid) and stage in f]
-        if len(pklmatch) > 0:
-            print('Found {:s}, {:f}, checking for plots'.format(kicid, δ))
-        else:
-            lcd, allq = ir.inject_transit_known_depth(lcd, δ)
-            lcd = ir.detrend_allquarters(lcd, σ_clip=30., inj=inj)
-            lcd = ir.normalize_allquarters(lcd, dt='dtr')
-            lcd = ir.iterative_whiten_allquarters(lcd, σ_clip=[30.,5.],
-                    nwhiten_max=nwhiten_max, nwhiten_min=nwhiten_min,
-                    rms_floor=0.0005)
-            if 'eb_sbtr' in stage:
-                kicid = ir.save_lightcurve_data(lcd, allq=allq, stage=stage)
-
-        # Load whitening output for visualization.
-        pltmatch = [f for f in
-                os.listdir('../results/eb_subtraction_diagnostics/iterwhiten') if
-                f.endswith('.png') and f.startswith(kicid)]
-        if len(pltmatch) > 0:
-            print('Found {:s}, plot'.format(kicid, δ))
-        else:
-            stage = origstage + '_' + str(δ)
-            lcd, loadfailed = ir.load_lightcurve_data(kicid, stage=stage)
-            if 'dipsearch' in stage or 'eb_sbtr' in stage:
-                allq, loadfailed = ir.load_allq_data(kicid, stage=stage)
-
-            # Make plots.
-            if 'iterwhiten' in stage or 'eb_sbtr' in stage:
-                irp.plot_iterwhiten_3row(lcd, allq, stage=stage)
-
-
 def injrecov_test1(N,
         whitened=True, ds=True,
         stage=None,
@@ -378,11 +38,11 @@ def injrecov_test1(N,
         iwplot=False,
         nwhiten_max=8,
         nwhiten_min=1):
-
     '''
     Inject transits and recover them on N entries from the Kepler Eclipsing
     Binary Catalog. There are two important objects:
     `lcd` organizes everything by quarter. `allq` stitches over all quarters.
+    Results get written to '../results/injrecovresult'
 
     Currently implemented:
 
@@ -390,15 +50,24 @@ def injrecov_test1(N,
             depth, anywhere from P_CBP=(4-80)x P_EB.
         detrend (legendre and [30,30]σ sigclip)->
         normalize (median by quarter)->
-        iterative whitening via PDM period-selection and legendre fitting->
+        iterative whitening via PDM period-selection and legendre fitting (both
+            in phase, then in time) ->
         find dips (BLS, over all the quarters)
 
     Args:
-        stage (str): 'dipsearch'.
+        stage (str): one of stages:
+            'pw' if post-whitening.
+            'redtr' if post-redetrending.
+            'dipsearch' if doing injection recovery.
+
         nwhiten_max (int): maximum number of iterative whitenings to do.
+
         nwhiten_min (int): minimum number of iterative whitenings to do.
 
-    See other docstrings for more descriptions.
+        inj (bool): True if you're injecting (fixes names of things).
+
+        whitened (bool), ds (bool): whether to create the whitened 6row and
+            dipsearch plots (both diagnostics), respectively.
     '''
 
     np.random.seed(N)
@@ -509,7 +178,8 @@ def pkls_to_results_csvs(stage='dipsearch', inj=True):
     stage = stage+'_inj' if inj else stage
     origstage = stage
 
-    # These files are all regenerated by this routine.
+    # These files are all regenerated by this routine. Since this is done by
+    # appending in irra.write_injrecov_result, we need to delete them.
     fs_to_rm = ['../results/injrecovresult/irresult_sap_top1.csv',
                 '../results/injrecovresult/irresult_sap_allN.csv',
                 '../results/injrecovresult/summary.txt']
@@ -527,44 +197,31 @@ def pkls_to_results_csvs(stage='dipsearch', inj=True):
         allq, loadfailed = ir.load_allq_data(kicid, stage=stage)
         if loadfailed:
             continue
-        # Write results tables. (Control flow logic: this is automatically
-        # done for any run. So we needed to delete the table before ANY
-        # run, and it'll reconstruct the table based on everything in the
-        # saved pickles.)
         irra.write_injrecov_result(lcd, allq, stage=stage)
 
     irra.summarize_injrecov_result()
 
 
-
-
-
-
 if __name__ == '__main__':
 
-    ## If you just need a quick `lcd` to play with:
-    #lcd = get_lcd(stage='dtr', inj=False)
-    #lcd, allq = get_lcd(stage='dipsearch', inj=True)
+    parser = argparse.ArgumentParser(
+        description='short period EB injrection-recovery (injection optional)')
+    parser.add_argument('-p', '--pkltocsv', action='store_true',
+        help='process all the pkl files made by injrecov to csv results')
+    parser.add_argument('-ir', '--injrecovtest', action='store_true',
+        help='run the inj&recov routines for a small number of trial stars')
+    parser.add_argument('-q', '--quicklcd', action='store_true',
+        help='if you need a quick `lcd` to play with, this option returns it')
 
-    ## Testing out injection:
-    #injrecov_fixed_transit_depth(100, stage='redtr', inj=True)
+    args = parser.parse_args()
 
-    ## Testing out recovery:
-    #injrecov_fixed_transit_depth(100, stage='dipsearch', inj=True)
+    if args.quicklcd:
+        lcd = get_lcd(stage='dtr', inj=False)
+        lcd, allq = get_lcd(stage='dipsearch', inj=True)
+        return lcd, allq
 
-    ## Run a "bonafide test" of the injection/recovery routines sufficient to
-    ## understand completeness vs various parameters (e.g., δ, P_CBP, RMS).
-    #injrecov_vary_depth(1000, stage='dipsearch', inj=True)
+    if args.injrecovtest:
+        injrecov_test1(103, stage='dipsearch', inj=True, ds=True, whitened=True)
 
-    ## Test iterative whitening (injects transits; no recovery)
-    #test_EB_subraction(100, stage='eb_sbtr', inj=True, ds=False, whitened=True)
-
-    ## Run a "bonafide test" of the injection/recovery routines, but now with
-    ## iterative whitening.
-    #injrecov_test1(103, stage='dipsearch', inj=True, ds=True, whitened=True)
-
-    ## If you need to process all the pickles to csv to write injrecov results.
-    pkls_to_results_csvs()
-
-    #TODO: make the above options arguments via parser
-    pass
+    if args.pkltocsv:
+        pkls_to_results_csvs()
