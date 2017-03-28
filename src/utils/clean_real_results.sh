927904ec01038fdfca61e70dfcb09343f46c56f9
lgbouma <lgbouma@mit.edu>
2017-03-28 11:07:37 -0400

    update utils (add clean_real_results)

---
+++ src/utils/clean_real_results.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm ../../results/eb_subtraction_diagnostics/real/*
+rm ../../results/whitened_diagnostic/real/*
+rm ../../results/dipsearchplot/real/*
+rm ../../results/real_search/*csv
+rm ../../results/real_search/*txt
