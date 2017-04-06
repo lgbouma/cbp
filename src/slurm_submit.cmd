c12813db317c43b28e66ca50117f1cc9ea34b746
lgbouma <lgbouma@mit.edu>
2017-04-06 15:11:05 -0400

    update slurm script for remaining morph>0.6 objs
    (This was run for these ~200 objects)

--- src/slurm_submit.cmd
+++ src/slurm_submit.cmd
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=find_dips
 #SBATCH --output=LOGS/%a_%A_slurm.out # %A: master job ID, %a: array tasks ID.
-#SBATCH --array=1-3
+#SBATCH --array=1-198
 #SBATCH -N 1   # node count. OpenMP requires 1.
-#SBATCH --ntasks-per-node=2  # core count. 2 per star, for testing.
-#SBATCH -t 0:04:30 # 5min gets test queue. e.g., 3:00:00 for 3hr
+#SBATCH --ntasks-per-node=1  # core count.
+#SBATCH -t 3:30:00 # 5min gets test queue. e.g., 3:00:00 for 3hr
 # sends mail when process begins, and
 # when it ends. Make sure you define your email
-#SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=lbouma@princeton.edu
 
@@ -20,6 +19,6 @@ source activate cbp
 # the job array. We'll use it to directly index our list of KIC IDs.
 
 linenumber=$SLURM_ARRAY_TASK_ID
-kicid="$(sed "${linenumber}q;d" ../data/morph_gt_0.6_ids.txt)"
+kicid="$(sed "${linenumber}q;d" ../data/not_done_ids.txt)"
 
-srun python run_the_machine.py -frd -c -kicid $kicid -nw 2 > LOGS/"$linenumber"_"$kicid".log
+srun python run_the_machine.py -frd -c -kicid $kicid -nw 1 > LOGS/"$linenumber"_"$kicid".log
